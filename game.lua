d98061e1bc964b35519a07c04c152d3b5af9445a
Aldo Utrera <aldo.utrera@travelogyx.com>
2015-04-29 17:15:35 -0500

    Initial commit

---
+++ game.lua
@@ -0,0 +1,100 @@
+require 'resource_definitions'
+require 'resource_manager'
+require 'input_manager'
+
+module("Game", package.seeall)
+
+local resource_definitions = {
+  background = {
+    type =  RESOURCE_TYPE_IMAGE,
+    fileName = 'images/parallax-mountain-bg.png',
+    width = 272, height = 160
+  },
+  farAway = {
+    type = RESOURCE_TYPE_IMAGE,
+    fileName = 'images/parallax-mountain-bg.png',
+    width = 625, height = 205
+  },
+  main = {
+    type = RESOURCE_TYPE_IMAGE,
+    fileName = 'images/parallax-mountain-bg.png',
+    width = 975, height = 171
+  },
+}
+
+local background_objects = {
+  background = {
+    position = { 0, 70 },
+    parallax = { 0.05, 0.05 }
+  },
+  farAway = {
+    position = { 0, 50 },
+    parallax = { 0.1, 0.1 }
+  },
+  main = {
+    position = { 0, -75 }
+    parallax = { 1, 1}
+  },
+}
+
+function Game:start()
+  self.initialize()
+
+  while(true) do
+    self:processInput()
+    coroutine.yield()
+  end
+end
+
+function Game:initialize()
+  self.camera = MOAICamera2D.new()
+  self.setupLayers()
+
+  ResourceDefinitions:setDefinitions(resource_definitions)
+
+  self.loadBackground()
+end
+
+function Game:loadBackground()
+  self.background = {}
+
+  for name, attributes in pairs(background_objects) do
+    local b = {}
+    b.deck = ResourceManager:get(name)
+    b.prop = MOAIProp2D.new()
+    b.prop:setDeck(b.deck)
+    b.prop:setLoc(unpack(attributes.position))
+
+    self.layers[name]:insertProp(b.prop)
+    self.layers[name]:setParallax(unpack(attributes.parallax))
+
+    self.background[name] = b
+  end
+
+end
+
+function Game:processInput()
+  local x, y = InputManager:deltaPosition()
+
+  self.camera:moveLoc(x, 0, 0.5, MOAIEaseType.LINEAR)
+end
+
+function Game:setupLayers()
+  self.layers = {}
+  self.layers.background = MOAILayer2D.new()
+  self.layers.farAway = MOAILayer2D.new()
+  self.layers.main = MOAILayer2D.new()
+
+  for key, layer in pairs(self.layers) do
+    layer:setViewport(viewport)
+    layer:setCamera(self.camera)
+  end
+
+  local renderTable = {
+    self.layers.background,
+    self.layers.farAway,
+    self.layers.main
+  }
+
+  MOAIRenderMgr.setRendereTable(renderTable)
+end
