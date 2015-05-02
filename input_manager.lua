171d58dd1a8147d2db28e2b219487f1893a7cf60
Aldo Utrera <aldo.utrera@travelogyx.com>
2015-05-02 13:50:36 -0500

    Commenting out the whole file. Adding the callback for the keyboard key press

--- input_manager.lua
+++ input_manager.lua
@@ -1,39 +1,51 @@
 module("InputManager", package.seeall)
 
-local pointerX, pointerY = 0, 0
-local previousX, previousY = 0, 0
+function InputManager.initialize()
 
-function InputManager:initialize()
-  if MOAIInputMgr.device.pointer then
-    local pointerCallback = function(x, y)
-      previousX, previewY = pointerX, pointerY
-      pointerX, pointerY = x, y
-
-      if touchCallbackFunc then
-        touchCallbackFunc(MOAITouchSensor.TOUCH_MOVE, 1, pointerX, pointerY, 1)
-      end
-    end
-
-    MOAIInputMgr.device.pointer:setCallback(pointerCallback)
+  function onKeyboardEvent(key, down)
+    if key == 119 then key = 'up' end
+    if key == 97 then key = 'left' end
+    if key == 100 then key = 'right' end
+    Game:keyPressed(key, down)
   end
+
+  MOAIInputMgr.device.keyboard:setCallback(onKeyboardEvent)
 end
 
-function InputManager:position()
-  return pointerX, pointerY
-end
+--local pointerX, pointerY = 0, 0
+--local previousX, previousY = 0, 0
 
-function InputManager:previousPosition()
-  return previousX, previousY
-end
+--function InputManager:initialize()
+--  if MOAIInputMgr.device.pointer then
+--    local pointerCallback = function(x, y)
+--      previousX, previewY = pointerX, pointerY
+--      pointerX, pointerY = x, y
 
-function InputManager:deltaPosition()
-  return pointerX - previousX, previousY - pointerY
-end
+--      if touchCallbackFunc then
+--        touchCallbackFunc(MOAITouchSensor.TOUCH_MOVE, 1, pointerX, pointerY, 1)
+--      end
+--    end
 
-function InputManager:isDown()
-  if MOAIInputMgr.device.touch then
-    return MOAIInputMgr.device.touch:isDown()
-  elseif MOAIInputMgr.device.pointer then
-    return (MOAIInputMgr.device.mouseLeft:isDown())
-  end
-end
+--    MOAIInputMgr.device.pointer:setCallback(pointerCallback)
+--  end
+--end
+
+--function InputManager:position()
+--  return pointerX, pointerY
+--end
+
+--function InputManager:previousPosition()
+--  return previousX, previousY
+--end
+
+--function InputManager:deltaPosition()
+--  return pointerX - previousX, previousY - pointerY
+--end
+
+--function InputManager:isDown()
+--  if MOAIInputMgr.device.touch then
+--    return MOAIInputMgr.device.touch:isDown()
+--  elseif MOAIInputMgr.device.pointer then
+--    return (MOAIInputMgr.device.mouseLeft:isDown())
+--  end
+--end
